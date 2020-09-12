bcac5e8934bb780d0c586fc4cf42a8db31e659d9
Brenno <brennoprogmm@gmail.com>
2020-09-12 10:33:28 -0300

    Finalização do Front e da validação do AddReceitas

--- Tempera/AddReceitas.php
+++ Tempera/AddReceitas.php
@@ -30,41 +30,44 @@
         <div class='Card'>
             <form autocomplete="off" method="POST" id="NovaReceita">
                 <div id='Name' class='Input-Row InputDefault' > 
-                    <input type='text' id='InputName' placeholder='Nome da receita' >    
+                    <input required  type='text' id='InputName' name='Inputname' placeholder='Nome da receita' >    
                 </div>
                 <div id='Image-Painel'> 
                     <input type='file' onchange='viewTargetImage()' name='image' id='image' class='Blue-Button'>
                     <label for='image' class='Blue-Button'>Adicionar Foto</label>
                 </div>
                  
-                <p class='opcional row' style='color:red'>Atenção! Colocar uma imagem ajuda muito na visibilidade da sua receita, mas não é obrigatório.</p> 
+                <p class='opcional row' style='color:green'>Atenção! Colocar uma imagem ajuda muito na visibilidade da sua receita, mas não é obrigatório.</p> 
                 <div id='Inputs-Painel'>
                     <div class='Input-Row' id='Row1'>
                         <!-- Linha de Tempo e Porções  -->
-                        <div class='row'>
-                            <p><i class="fas fa-clock"></i><label for='Time'>Tempo</label></p>
-                            <input pattern='[0-9]{1,3}' maxlength="3" class='Basic-Input' type='text' name='Time' id='Time'>
-                            <spam class='desc'>Minutos</spam>
+                        <div class='row' id='align'>
+                            <div class='row'>
+                                <p><i class="fas fa-clock"></i><label for='Time'>Tempo</label></p>
+                                <input required pattern='[0-9]{1,3}' maxlength="3" class='Basic-Input' type='text' name='Time' id='Time'>
+                                <spam class='desc'>Minutos</spam>
+                            </div>
+                            <div class='row'>
+                                <p><i class="fas fa-weight-hanging"></i><label for='Calorias'>Calorias</label></p>
+                                <input  pattern='[0-9]{1,4}' maxlength="4" class='Basic-Input' type='text' name='Calorias' id='Calorias'>
+                                <spam class='desc'>Cal</spam>
+                            </div>
+                            <div class='row'>
+                                <p><i class="fas fa-user-friends"></i><label for='Qtdd-Porcoes'>Porções</label></p>
+                                <input required type='text' pattern='[0-9]{1,2}' maxlength="2" class='Basic-Input' name='QtddPorcoes' id='Qtdd-Pocoes'>
+                                <spam class='desc'>Porções</spam>
+                            </div>
                         </div>
-                        <div class='row'>
-                            <p><i class="fas fa-weight-hanging"></i><label for='Time'>Calorias</label></p>
-                            <input pattern='[0-9]{1,4}' maxlength="4" class='Basic-Input' type='text' name='Calorias' id='Calorias'>
-                            <spam class='desc'>Cal</spam>
-                        </div>
-                        <div class='row'>
-                            <p><i class="fas fa-user-friends"></i><label for='Qtdd-Porcoes'>Porções</label></p>
-                            <input type='text' pattern='[0-9]{1,2}' maxlength="2" class='Basic-Input' name='Qtdd-Porcoes' id='Qtdd-Pocoes'>
-                            <spam class='desc'>Porções</spam>
-                        </div>
+                        <span class='opcional row'>Opcional</span> 
+                        <span class='opcional warning row'></span>                         
                     </div>
-                    <span class='opcional row'>Opcional</span> 
 
                     <div class='Input-Row' id='Row2'>
                         <div class='row'>
                             
                             <label for='IngredientesText'><i class="fas fa-carrot"></i>Ingredientes</label>
                             <span class='desc'>O formato do texto será mantido na exibição da receita. Escreva de forma organizada.</span>
-                            <textarea spellcheck="false" id='IngredientesText'> </textarea>
+                            <textarea spellcheck="false" name='Ingredientes' id='IngredientesText'> </textarea>
                         </div>
                     </div>
 
@@ -73,7 +76,7 @@
                             
                             <label for='IngredientesText'><i class="fas fa-concierge-bell"></i>Modo de Preparo</label>
                             <span class='desc'>O formato do texto será mantido na exibição da receita. Escreva de forma organizada.</span>
-                            <textarea spellcheck="false" id='PreparoText'> </textarea>
+                            <textarea spellcheck="false" name='Preparo' id='PreparoText'> </textarea>
                         </div>
                     </div>
 
@@ -81,7 +84,7 @@
                         <div class='row'>
                             
                             <label for='Tags'><i class="fas fa-tag"></i>Tag</label>
-                            <select id='Tags'> 
+                            <select id='Tags' name='Tags'> 
                                 <option value='Tag0'>Selecione uma Tag</option>
                                 <option value='Tag1'>Carnes</option>
                                 <option value='Tag2'>Fitness</option>
@@ -96,6 +99,8 @@
                             <button class='Blue-Button' form='NovaReceita'>Enviar Receita</button>
                         </div>
                     </div>
+
+                    <p class='opcional warning row' id='align' name='submitError'></p> 
                 </div>
              
             
--- Tempera/JS/AddReceita.js
+++ Tempera/JS/AddReceita.js
@@ -1,14 +1,60 @@
 const log = console.log
-const imgPainel = document.querySelector('#image');
-const imgLabel = document.querySelector("#Image-Painel")
-function viewTargetImage(){
-    // Vai pegar a imagem que fou upada
-    const img = imgPainel.files[0]
-    // Vai transformar a imagem em uma URL
-    const imgURL = URL.createObjectURL(img)
 
-    imgLabel.style.backgroundImage = `url(${imgURL}`
+const form = document.querySelector('form')
+const inputs = form.querySelectorAll('input')
+
+function noInputErrorIndicator(){
+    const ingredientes = form.querySelector('#IngredientesText')
+    const preparo = form.querySelector('#PreparoText')
+    const tags = form.querySelector('#Tags')
+    fields = [
+        ingredientes,
+        preparo,
+        tags
+    ]
+   
+    for(i = 0; i < fields.length; i++){
+        if(fields[i].value == " " || fields[i].value == "Tag0" || fields[i].value == null || fields[i].value == "" ){
+            fields[i].scrollIntoView()
+            fields[i].style.border = '2px solid red'
+            log(fields[i], fields[i].value)
+        }else{
+            fields[i].style.border = ''
+        }
+    }
 
 }
 
+form.addEventListener(
+    'submit',
+    (event) =>{
+        event.preventDefault(),
+        noInputErrorIndicator(event)
+})
 
+function setErrorIndicator(event) {
+    const input = event.target
+    
+    if(!input.validity.valid){
+        input.scrollIntoView()
+        input.style.border = '2px solid red'
+    } else{
+        input.style.border = ''
+    }
+   
+}
+
+for(input of inputs){
+    input.addEventListener(
+    'invalid', 
+    (event) => {
+        event.preventDefault();
+        setErrorIndicator(event)
+    })
+    input.addEventListener(
+    'blur',
+    (event) => {
+    setErrorIndicator(event)
+    }
+    )
+}
